1cfc562a96dde44dea3638710fa25fb29fb54858
Jpfirmo <127751417+jpfirmo@users.noreply.github.com>
2023-04-09 09:44:08 -0300

    Add files via upload

---
+++ exercicio_06.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class exercicio_06 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner entrada = new Scanner (System.in);
+		System.out.println("digite um número: ");
+		int number = entrada.nextInt();
+		if(number % 3 == 0) {
+			System.out.println("é multiplo de três");
+		}else {
+			System.out.println("não é multiplo de três");
+		}
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
